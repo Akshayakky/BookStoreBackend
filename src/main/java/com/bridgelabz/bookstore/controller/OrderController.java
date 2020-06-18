0b1b94208a213e74f34d337260fd574f0209a0e3
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-06-18 13:11:16 +0530

    ADDED: Controller for Order details api

---
+++ src/main/java/com/bridgelabz/bookstore/controller/OrderController.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.bookstore.controller;
+
+import com.bridgelabz.bookstore.dto.OrderDTO;
+import com.bridgelabz.bookstore.model.MyOrder;
+import com.bridgelabz.bookstore.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@CrossOrigin
+@RequestMapping("/order")
+public class OrderController {
+
+    @Autowired
+    IOrderService iOrderService;
+
+    /**
+     * @param orderDTO
+     * @return
+     */
+    @PostMapping
+    public MyOrder addOrder(@RequestBody OrderDTO orderDTO) {
+        return iOrderService.addOrder(orderDTO);
+    }
+
+    /**
+     * @param userId
+     * @return All order details of user
+     */
+    @GetMapping("/{userId}")
+    public List<MyOrder> getAllOrders(@PathVariable long userId) {
+        return iOrderService.getAllOrders(userId);
+    }
+
+}
