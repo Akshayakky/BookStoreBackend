e61902caa982a8041f4ec1df996e65b761cee16b
ajsushant1 <sushantajgaonkar1766@gmail.com>
2020-06-18 13:15:28 +0530

    ADDED: Repository with method custom method to get all Order details

---
+++ src/main/java/com/bridgelabz/bookstore/repository/OrderRepository.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.bookstore.repository;
+
+import com.bridgelabz.bookstore.model.MyOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OrderRepository extends JpaRepository<MyOrder, Long> {
+    List<MyOrder> findAllByUser(long userId);
+
+}
