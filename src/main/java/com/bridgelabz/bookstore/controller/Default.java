329b1290b33355bf69e7a051380511ede6d78aa8
akshayakky <akshaysmpt01@gmail.com>
2020-07-16 18:10:49 +0530

    ADDED : Defautl ccontroller

--- src/main/java/com/bridgelabz/bookstore/controller/Default.java
+++ src/main/java/com/bridgelabz/bookstore/controller/Default.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 @CrossOrigin
 public class Default {
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getMessage() {
         return "Hello";
     }
